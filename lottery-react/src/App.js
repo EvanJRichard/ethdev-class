cf7e63ed88ea6d7e009471c952462224566da840
Evan <none>
2018-08-05 17:52:46 -0400

    Submit a transaciton when the Enter button is clicked.

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -18,6 +18,17 @@ class App extends Component {
     const balance = await web3.eth.getBalance(lottery.options.address);
     this.setState({ manager, players, balance });
   }
+
+  onSubmit = async formSubmissionEvent => {
+    formSubmissionEvent.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether")
+    });
+  };
+
   render() {
     return (
       <div>
@@ -29,7 +40,7 @@ class App extends Component {
         </p>
 
         <hr />
-        <form>
+        <form onSubmit={this.onSubmit}>
           <h4> Want to try your luck?</h4>
           <div>
             <label>Amount of ether to enter</label>

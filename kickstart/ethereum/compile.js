d3dd0d2cff6751d36e97f43c471f53f6402b654b
Evan <none>
2018-08-06 20:54:43 -0400

    Add compiler script.

---
+++ kickstart/ethereum/compile.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const fs = require("fs-extra");
+const solc = require("solc");
+
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath); //rm build path for clean build
+
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8"); //read the contract
+const compilerOutput = solc.compile(source, 1).contracts; //contains both the campaign and the campaignfactory
+
+fs.ensureDirSync(buildPath);
+
+//take each contract in compilerOutput and put it in build
+for (let contract in compilerOutput) {
+	fs.outputJsonSync(
+		path.resolve(buildPath, contract + ".json"),
+		compilerOutput[contract]
+	);
+}
